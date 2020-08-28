04268f8a29337c5d5f360b6b78729dca62ff1185
NargesGhasemi <59624936+NargesGhasemi@users.noreply.github.com>
2020-08-28 07:48:26 -0700

    add handler.js

---
+++ server/handlers.js
@@ -0,0 +1,45 @@
+var writeFile = require("./writeFile").writeHandler,
+    readFile = require("./readFile").readHandler,
+    signUpFile = require("./signupFile").signUpHandler,
+    signInFile = require("./signInFile").signInHandler,
+    staticFilesHandler = require("./staticFilesHandler").staticFilesHandler,
+    decodeUser = require("./decoder").decodeUser,
+    validateToken = require("./validToken").validateToken;
+
+exports.routeHandlers = function (req, res) {
+    console.log("req.url: ", req.url);
+    console.log("req.method: ", req.method);
+    var routes;
+
+    if (req.method === "GET" && req.url === "/read") {
+        var jwt = req.headers.jwt;
+        if (!validateToken(jwt)) {
+            res.writeHead(401);
+            res.write('sorry You have not Access!');
+            res.end();
+            return;
+        }
+        routes = readFile;
+    }
+    else if (req.method === "POST" && req.url === "/write") {
+        var jwt = req.headers.jwt;
+        if (!validateToken(jwt)) {
+            res.writeHead(401);
+            res.write('sorry You have not Access!');
+            res.end();
+            return;
+        }
+        routes = writeFile;
+    }
+    else if (req.method === "POST" && req.url === "/signUp") {
+        routes = signUpFile;
+    }
+    else if (req.method === "POST" && req.url === "/signIn") {
+        routes = signInFile;
+    }
+    else {
+        routes = staticFilesHandler;
+    }
+
+    routes(req, res);
+};
